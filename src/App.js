857317f1f8d994e0875b59edd82b23a9fb46bf72
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-05-26 12:02:04 +0100

    newconstantdone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showOriginal, setShowOriginal] = useState(true);
+  const [boolean, setBoolean] = useState("undefined");
+  const [argument,setArgument]=useState()
+
+  const handleBoolean = (event) => {
+    setBoolean(event.target.value);
+  };
+
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+    setShowOriginal(false);
+  };
+
+  const handleClose = () => {
+    setShowOriginal(true);
+    setSelectedOption("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showOriginal ? (
+        <Original handleOptionChange={handleOptionChange} />
+      ) : selectedOption === "constant" ? (
+        <Boolean handleClose={handleClose} handleBoolean={handleBoolean} />
+      ) : selectedOption === "argument" ? (
+        <Argument handleClose={handleClose} />
+      ) : selectedOption === "and" ? (
+        <And handleClose={handleClose} />
+      ) : selectedOption === "or" ? (
+        <Or handleClose={handleClose} />
+      ) : null}
+
+      <div>result:{boolean}</div>
     </div>
   );
 }
 
+function Original({ handleOptionChange }) {
+  return (
+    <>
+      <select onChange={handleOptionChange}>
+        <option value="">-- select --</option>
+        <option value="constant">Constant</option>
+        <option value="argument">Argument</option>
+        <option value="and">And</option>
+        <option value="or">Or</option>
+      </select>
+      <button>X</button>
+    </>
+  );
+}
+
+function Boolean({ handleClose,handleBoolean }) {
+  return (
+    <>
+       <select onChange={handleBoolean}>
+      <option value="true">true</option>
+      <option value="false">false</option>
+    </select>
+      <button onClick={handleClose}>X</button>
+    </>
+  );
+}
+
+function Argument({ handleClose }) {
+  return (
+    <>
+      <div>Argument Component</div>
+      <button onClick={handleClose}>X</button>
+    </>
+  );
+}
+
+function And({ handleClose }) {
+  return (
+    <>
+      <div>And Component</div>
+      <button onClick={handleClose}>X</button>
+    </>
+  );
+}
+
+function Or({ handleClose }) {
+  return (
+    <>
+      <div>Or Component</div>
+      <button onClick={handleClose}>X</button>
+    </>
+  );
+}
+
+function UpperArgument(){
+  <>
+  <input/>
+  <Boolean/>
+  </>
+}
+
 export default App;
+
